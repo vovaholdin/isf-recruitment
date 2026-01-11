f7949d231c928e737954465f3638c8e6939adc37
Volodymyr <holdinvolodymyr@gmail.com>
2026-01-11 23:56:44 +0000

    add security filterChain

---
+++ src/main/java/com/company/isf/common/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.company.isf.common.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/hr/**").hasRole("HR")
+                                .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/vacancy")
+                        .failureUrl("/hr/login?error=true")
+                        .permitAll()
+                );
+        return httpSecurity.build();
+    }
+}
