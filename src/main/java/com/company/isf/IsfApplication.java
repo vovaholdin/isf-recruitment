76eaa57dbb2cb6bddb5e7bbc7c88a66d450c3651
Volodymyr <holdinvolodymyr@gmail.com>
2026-01-11 16:45:41 +0000

    initial project setup

---
+++ src/main/java/com/company/isf/IsfApplication.java
@@ -0,0 +1,13 @@
+package com.company.isf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/isf/IsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.isf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
